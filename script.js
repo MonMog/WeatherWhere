d4767f11f1f6adaba8a5ce7daed50e26827afd1e
MoneebMog <122757829+MoneebMog@users.noreply.github.com>
2023-01-17 00:23:07 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const searchBtn = document.getElementById("search-btn");
+const locationInput = document.getElementById("location");
+const weatherData = document.getElementById("weather-data");
+
+searchBtn.addEventListener("click", async (e) => {
+  e.preventDefault();
+  const location = locationInput.value;
+  const weatherAPI = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.WEA_API_KEY}&units=metric`;
+  weatherData.innerHTML = `<p>Loading...</p>`;
+  try {
+    const response = await fetch(weatherAPI);
+    const data = await response.json();
+    const main = data.main;
+    const weather = data.weather[0];
+  
+    weatherData.innerHTML = `
+      <h2>Weather in ${data.name}</h2>
+      <p>Temperature: ${main.temp}&#8451;</p>
+      <p>Feels like: ${main.feels_like}&#8451;</p>
+      <p>Weather condition: ${weather.main} - ${weather.description}</p>
+      <p>Humidity: ${main.humidity}%</p>
+      <p>Wind Speed: ${data.wind.speed} m/s</p>
+    `;
+  } catch (err) {
+    console.error(err);
+    weatherData.innerHTML = `<p>An error occurred, please try again later.</p>`;
+  }
+});
+
+
